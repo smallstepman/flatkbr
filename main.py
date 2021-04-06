f9030c5de6165f55243e2474033484878a3cda59
Marcin <m.liebiediew@gmail.com>
2021-04-06 23:15:06 +0200

    keyswitch housing V2 - testing multiple thicknesses and keyswitch housing depths

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 import solid as sl
 
 
-def choc_switch():
+def choc_switch(test=5):
     """
     Kailh Choc switch V1
     """
     o = offset = 3
-    m = margin = 1.01
-    body_xyz = [14.5 + m, 13.5 + m, 2]
+    m = margin = 1.02
+    body_xyz = [14.5 + m, 13.5 + m, test]
     socket_xyz = [body_xyz[0] - 1, body_xyz[1] - 1, 5.55 + o]
     # keycaphole_xyz = [1.2, 3.0, None]
     # distance_between_center_of_keycap_holes = 5.7
@@ -32,5 +32,14 @@ def plate():
     return p
 
 
+gg = 19
 keyboard_base = plate() - choc_switch()
-sl.scad_render_to_file(keyboard_base, "model.scad")
+keyboard_base1 = sl.translate([2 * gg, 0, 0])(plate() - choc_switch(test=6))
+keyboard_base2 = sl.translate([gg, 0, 0])(plate() - choc_switch(test=4))
+keyboard_base3 = sl.translate([-gg, 0, 0])(plate() - choc_switch(test=3))
+
+# keyboard_base = choc_switch()
+sl.scad_render_to_file(
+    keyboard_base + keyboard_base2 + keyboard_base3 + keyboard_base1,
+    "things/model.scad",
+)
